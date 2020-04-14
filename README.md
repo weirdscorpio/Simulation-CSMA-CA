22c3c10e8893e47309d698b16825a8f84754f386
weirdscorpio <43347459+weirdscorpio@users.noreply.github.com>
2020-04-14 12:28:45 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Data-Communication-Assignment
+A website which explains Carrier Sense Multiple Access With Collision Avoidance ( CSMA / CA ) with a live simulation
---
+++ simulation.js
@@ -0,0 +1,146 @@
+/*variables*/
+
+var x;
+var y;
+var ifs = 4000;
+var k = [0,0,0,0,0,0];
+var tp = 1000;
+var channel = 0;
+var r;
+var clsrts=document.querySelectorAll(".rts");
+var clscts=document.querySelectorAll(".cts");
+var clsdata=document.querySelectorAll(".sending-data");
+
+
+/*variables end*/
+
+/*functions*/
+
+
+function display(a,b){
+  x=a;
+  y=b;
+  checkChannel();
+}
+
+function checkChannel(){
+  if(channel == 0 )
+  {
+    setTimeout(sendSignal,ifs);
+  }
+  else
+  {
+    alert("Transmission taking place,channel not idle");
+  }
+}
+
+function sendSignal(){
+  r = tp*parseInt(Math.random()*k[x-1]);
+  sendRTS();
+}
+
+function sendRTS()
+{ var a = "station"+ x;
+  clsrts[x-1].innerHTML="RTS : Sent";
+  clsrts[y-1].innerHTML="RTS : Received";
+  var n = Math.random();
+  if(n>0.85)
+  {
+    k[x-1]=k[x-1]+1;
+    if(k[x-1]>15)
+    {
+      abortSignal();
+    }
+    else{
+      clscts[x-1].innerHTML="CTS : Not received,trying again";
+      clscts[y-1].innerHTML="CTS : Could not send";
+      setTimeout(checkChannel,tp*r);
+    }
+  }
+  else{
+    checkIFS();
+  }
+}
+
+function checkIFS(){
+  for(let n=0;n<6;n++)
+  {
+    if(n!=y-1)
+      clscts[n].innerHTML="CTS : Received";
+  }
+  document.getElementById("station1").innerHTML="S"+x;
+  document.getElementById("station2").innerHTML="S"+y;
+  clscts[y-1].innerHTML="CTS : Sent"
+  clsdata[x-1].innerHTML="Data Transfer : Sending";
+  clsdata[y-1].innerHTML="Data Transfer : Receiving";
+  channel = 1;
+  document.getElementById("channel").innerHTML="Channel : Not free";
+  swap();
+  setTimeout(sendFrame,ifs);
+}
+
+function swap(){
+  var none = document.querySelectorAll(".d-none");
+  var block = document.querySelectorAll(".d-flex");
+  none[0].classList.remove('d-none');
+  none[0].classList.add('d-flex');
+  block[0].classList.remove('d-flex');
+  block[0].classList.add('d-none');
+}
+
+function sendFrame(){  
+  var n = Math.random();
+  if(n>0.1)
+  {
+    success();
+  }
+  else{
+    k[x-1]=k[x-1]+1;
+    if(k[x-1]>15)
+    { swap();
+      abortSignal();
+    }
+    else{
+      clsdata[x-1].innerHTML=clsdata[y-1].innerHTML="Acknowledge timeout,trying again";
+      channel = 0; 
+      for(let i =0;i<6;i++)
+      {
+        clscts[i].innerHTML="CTS : -";
+        clsrts[i].innerHTML="CTS : -";
+        document.getElementById("channel").innerHTML="Channel : Free";
+        clsdata[i].innerHTML="Data Transfer : -";
+      }
+      document.getElementById("channel").innerHTML="Channel : Free";
+      setTimeout(checkChannel,tp*r);
+    }
+  } 
+}
+
+function success(){
+  clsdata[x-1].innerHTML=clsdata[y-1].innerHTML="Data sent successfully!";
+  alert("Data sent successfully!");
+  swap();
+  setDefault();
+}
+
+function abortSignal(){
+  clsdata[x-1].innerHTML=clsdata[y-1].innerHTML="Data not sent,process aborted";
+  setDefault();
+  alert("Data not sent,process aborted");
+}
+
+function setDefault(){
+  k = [0,0,0,0,0,0];
+  r = 0;
+  channel = 0;
+  for(let i =0;i<6;i++)
+  {
+    clscts[i].innerHTML="CTS : -";
+    clsrts[i].innerHTML="RTS : -";
+    document.getElementById("channel").innerHTML="Channel : Free";
+    clsdata[i].innerHTML="Data Transfer : -";
+  }
+}
+
+
+/*functions end*/
